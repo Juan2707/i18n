0a330a6712c4726223b4d9474356a8668d16877f
Juan2707 <ju4nv3rgara@gmail.com>
2022-11-22 09:14:22 -0500

    ejercicio clase

--- src/components/job.js
+++ src/components/job.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { FormattedDate, FormattedNumber, FormattedMessage } from 'react-intl';
+
 
 const Job = (props) => {
   return (
@@ -6,9 +8,34 @@ const Job = (props) => {
       <th scope="row">{props.offer.id}</th>
       <td>{props.offer.name}</td>
       <td>{props.offer.company}</td>
-      <td>{props.offer.salary}</td>
+      <td>
+        {props.offer.salary} millones
+          
+      </td>
       <td>{props.offer.city}</td>
-      <td>{props.offer.date}</td>
+      <td>
+        <FormattedDate
+          value={new Date(props.offer.date)}
+          year='numeric'
+          month='long'
+          day='numeric'
+          weekday='long'
+        />
+      </td>
+      <td>
+        <FormattedNumber
+          value={new Number(props.offer.views)}
+          locale="en-US"
+          maximumFractionDigits={3}
+          minimumFractionDigits={0}
+          minimumIntegerDigits={1}
+          notation="standard"
+          numberingSystem="latn"
+          signDisplay="auto"
+          style="decimal"
+          useGrouping="always"          
+        />
+      </td>
     </tr>
   );
 };
